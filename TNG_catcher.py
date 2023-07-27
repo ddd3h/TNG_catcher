e943d42a7f993c9265e1614c61c0be56453e7919
ddd3h <powerfulnight63d.daisuke@gmail.com>
2023-07-27 10:35:53 +0900

    feat: Compatible with faceon, edgeon, etc.

--- TNG_catcher.py
+++ TNG_catcher.py
@@ -5,8 +5,9 @@ try:
     import os
     import matplotlib.pyplot as plt
     from matplotlib.colors import LogNorm
+    import glob
 
-            
+
 except ModuleNotFoundError:
     print("The following modules is not installed at least:")
     print("\t- illustris_python")
@@ -23,6 +24,8 @@ else:
     print("\t- import os")
     print("\t- import matplotlib.pyplot as plt")
     print("\t- from matplotlib.colors import LogNorm")
+    print("\t- import glob")
+
 
 class extract_subhalo_data:
     def __init__(self, basePath):
@@ -36,15 +39,19 @@ class extract_subhalo_data:
         self.fields = ("Coordinates", "Masses")
         self.__fields = ("x", "y", "z", "Masses")
         self.snapnum = 99
-        
-    def change_subhalo_mode(self, is_subhalo = True):
+
+    def change_subhalo_mode(self, is_subhalo=True):
         self.__is_subhalo = is_subhalo
         if self.__is_subhalo:
             print("Mode: Subhalo")
-            print("The only ID to set with `get_list` and `set_list` is the Subhalo ID; if you want to make it a Halo ID, change it to **False**.")
+            print(
+                "The only ID to set with `get_list` and `set_list` is the Subhalo ID; if you want to make it a Halo ID, change it to **False**."
+            )
         else:
             print("Mode: Halo")
-            print("The only ID to set with `get_list` and `set_list` is the Halo ID; if you want to make it a Subhalo ID, change it to **True**.")
+            print(
+                "The only ID to set with `get_list` and `set_list` is the Halo ID; if you want to make it a Subhalo ID, change it to **True**."
+            )
 
     def get_list(self, filename):
         with open(filename, mode="r") as f:
@@ -70,7 +77,18 @@ class extract_subhalo_data:
                 _fields.append("z")
                 self.__in_coordinates = True
             elif i == "GFM_Metals":
-                for j in ["H","He","C","N","O","Ne","Mg","Si","Fe","MetalTotal"]:
+                for j in [
+                    "H",
+                    "He",
+                    "C",
+                    "N",
+                    "O",
+                    "Ne",
+                    "Mg",
+                    "Si",
+                    "Fe",
+                    "MetalTotal",
+                ]:
                     _fields.append(f"{j}")
             else:
                 _fields.append(i)
@@ -94,7 +112,7 @@ class extract_subhalo_data:
             raise ValueError("output_file_format must be 'npz' or 'pickel', 'csv'.")
         self.output_file_format = output_file_format
 
-    def run(self):
+    def run(self, cut=False):
         os.makedirs(self.output_dirname, exist_ok=True)
         for i in self.subhalolist:
             if self.__is_subhalo:
@@ -157,14 +175,18 @@ class extract_subhalo_data:
                     df_dict[j] = np.array(sunhaloData[j])
 
             df = pd.DataFrame(df_dict)
-            df = df[
-                (pos[0] - dis < df["x"])
-                & (df["x"] < pos[0] + dis)
-                & (pos[1] - dis < df["y"])
-                & (df["y"] < pos[1] + dis)
-                & (pos[2] - dis < df["z"])
-                & (df["z"] < pos[2] + dis)
-            ].reset_index(drop=True)
+
+            if cut:
+                pass
+            else:
+                df = df[
+                    (pos[0] - dis < df["x"])
+                    & (df["x"] < pos[0] + dis)
+                    & (pos[1] - dis < df["y"])
+                    & (df["y"] < pos[1] + dis)
+                    & (pos[2] - dis < df["z"])
+                    & (df["z"] < pos[2] + dis)
+                ].reset_index(drop=True)
 
             if self.output_file_format == "npz":
                 df_dict = {}
@@ -179,7 +201,9 @@ class extract_subhalo_data:
 
     def info(self):
         print("fields: ", self.fields)
-        print("## You can set fields of here: https://www.tng-project.org/data/docs/specifications/#parttype0")
+        print(
+            "## You can set fields of here: https://www.tng-project.org/data/docs/specifications/#parttype0"
+        )
         print("")
         print("basePath: \t\t", self.basePath)
         print("snapnum: \t\t", self.snapnum)
@@ -193,35 +217,49 @@ class extract_subhalo_data:
 
 class plot_tools:
     def __init__(self, data):
-        self.__data = dict(data)
+        self.data = dict(data)
+
         self.__files_list = data.files
         self.__rotation_ok = False
 
-        if all(elem in self.__files_list for elem in ('x', 'y', 'z')):
+        if all(elem in self.__files_list for elem in ("x", "y", "z")):
             self.__rotation_ok = True
             self.__rotation_record = []
 
         # self.__file_format = data.split(".")[-1]
-    
-    def simple_plot(self,x,y,z,bins=301,log=True,fast=False, savefig=""):
-        x,y,z = self.__data[x], self.__data[y], self.__data[z]
+
+    def simple_plot(self, x, y, z, bins=301, log=True, fast=False, savefig=""):
+        x, y, z = self.data[x], self.data[y], self.data[z]
+        if z in ["Density"]:
+            print(
+                "Because it is not a quantifiable variable, what is drawn is not exactness."
+            )
         xbins = np.linspace(x.min(), x.max(), bins)
         ybins = np.linspace(y.min(), y.max(), bins)
         hist, xed, yed = np.histogram2d(x, y, bins=(xbins, ybins), weights=z)
         if fast:
             if log:
-                plt.imshow(hist.T, origin='lower', norm=LogNorm(vmin=hist[hist>0].min(), vmax=hist.max()))
+                plt.imshow(
+                    hist.T,
+                    origin="lower",
+                    norm=LogNorm(vmin=hist[hist > 0].min(), vmax=hist.max()),
+                )
                 plt.axis("off")
                 plt.colorbar()
                 plt.show()
             else:
-                plt.imshow(hist.T, origin='lower')
+                plt.imshow(hist.T, origin="lower")
                 plt.axis("off")
                 plt.colorbar()
                 plt.show()
         else:
             if log:
-                plt.pcolormesh(xed, yed, hist.T, norm=LogNorm(vmin=hist[hist>0].min(), vmax=hist.max()))
+                plt.pcolormesh(
+                    xed,
+                    yed,
+                    hist.T,
+                    norm=LogNorm(vmin=hist[hist > 0].min(), vmax=hist.max()),
+                )
                 plt.colorbar()
                 plt.show()
             else:
@@ -232,7 +270,12 @@ class plot_tools:
         if savefig:
             counter = 0
             if log:
-                plt.pcolormesh(xed, yed, hist.T, norm=LogNorm(vmin=hist[hist>0].min(), vmax=hist.max()))
+                plt.pcolormesh(
+                    xed,
+                    yed,
+                    hist.T,
+                    norm=LogNorm(vmin=hist[hist > 0].min(), vmax=hist.max()),
+                )
                 plt.colorbar()
                 while os.path.exists(savefig):
                     counter += 1
@@ -247,82 +290,152 @@ class plot_tools:
                     savefig = f"{counter}{savefig}"
                 plt.savefig(savefig)
 
-
     def rot_x(self, theta):
         if self.__rotation_ok:
-            theta_x = np.radians(theta)
-            positions = np.array([self.__data['x'], self.__data['y'], self.__data['z']])
-            rot_x = np.array([[ 1,                 0,                  0],
-                          [ 0, np.cos(theta_x), -np.sin(theta_x)],
-                          [ 0, np.sin(theta_x),  np.cos(theta_x)]])
-            self.__data['x'], self.__data['y'], self.__data['z'] = rot_x @ positions
-            self.__rotation_record.append(('x',theta))
+            positions = np.array([self.data["x"], self.data["y"], self.data["z"]])
+            self.data["x"], self.data["y"], self.data["z"] = (
+                self.__rot_x(theta) @ positions
+            )
+            self.__rotation_record.append(self.__rot_x(theta))
 
     def rot_y(self, theta):
         if self.__rotation_ok:
-            theta_y = np.radians(theta)
-            positions = np.array([self.__data['x'], self.__data['y'], self.__data['z']])
-            rot_y = np.array([[ np.cos(theta_y), 0,  np.sin(theta_y)],
-                          [                 0, 1,                  0],
-                          [-np.sin(theta_y), 0, np.cos(theta_y)]])
-            self.__data['x'], self.__data['y'], self.__data['z'] = rot_y @ positions
-            self.__rotation_record.append(('y',theta))
+            positions = np.array([self.data["x"], self.data["y"], self.data["z"]])
+            self.data["x"], self.data["y"], self.data["z"] = (
+                self.__rot_y(theta) @ positions
+            )
+            self.__rotation_record.append(self.__rot_y(theta))
 
     def rot_z(self, theta):
         if self.__rotation_ok:
-            theta_z = np.radians(theta)
-            positions = np.array([self.__data['x'], self.__data['y'], self.__data['z']])
-            rot_z = np.array([[ np.cos(theta_z), -np.sin(theta_z), 0],
-                          [ np.sin(theta_z),  np.cos(theta_z), 0],
-                          [                 0,                  0, 1]])
-            self.__data['x'], self.__data['y'], self.__data['z'] = rot_z @ positions
-            self.__rotation_record.append(('z',theta))
-    
-    def __rot_x(self,theta):
-        theta = np.radians(theta)
-        return np.array([[ 1,                 0,                  0],
-                          [ 0, np.cos(theta), -np.sin(theta)],
-                          [ 0, np.sin(theta),  np.cos(theta)]])
-    
-    def __rot_y(self,theta):
-        theta = np.radians(theta)
-        return np.array([[ np.cos(theta), 0,  np.sin(theta)],
-                          [                 0, 1,                  0],
-                          [-np.sin(theta), 0, np.cos(theta)]])
-    
-    
-    def __rot_z(self,theta):
-        theta = np.radians(theta)
-        return np.array([[ np.cos(theta), -np.sin(theta), 0],
-                          [ np.sin(theta),  np.cos(theta), 0],
-                          [                 0,                  0, 1]])
+            positions = np.array([self.data["x"], self.data["y"], self.data["z"]])
+            self.data["x"], self.data["y"], self.data["z"] = (
+                self.__rot_z(theta) @ positions
+            )
+            self.__rotation_record.append(self.__rot_z(theta))
 
+    def __rot_x(self, theta):
+        theta = np.radians(theta)
+        return np.array(
+            [
+                [1, 0, 0],
+                [0, np.cos(theta), -np.sin(theta)],
+                [0, np.sin(theta), np.cos(theta)],
+            ]
+        )
 
-    def save_rot(self,filename):
+    def __rot_y(self, theta):
+        theta = np.radians(theta)
+        return np.array(
+            [
+                [np.cos(theta), 0, np.sin(theta)],
+                [0, 1, 0],
+                [-np.sin(theta), 0, np.cos(theta)],
+            ]
+        )
+
+    def __rot_z(self, theta):
+        theta = np.radians(theta)
+        return np.array(
+            [
+                [np.cos(theta), -np.sin(theta), 0],
+                [np.sin(theta), np.cos(theta), 0],
+                [0, 0, 1],
+            ]
+        )
+
+    def save_rot(self, filename):
         if self.__rotation_ok:
-            # with open(filename,'w') as f:
-            #     for i in self.__rotation_record:
-            #         f.write(f"{str(i)}\n")
-            #     f.close()
             a = np.eye(3)
             for i in self.__rotation_record:
-                if i[0] == 'x':
-                    a = a @ self.__rot_x(i[1])
-                elif i[0] == 'y':
-                    a = a @ self.__rot_y(i[1])
-                elif i[0] == 'z':
-                    a = a @ self.__rot_z(i[1])
-            np.save(filename,a)
+                a = a @ i
+            np.save(filename, a)
 
-
-    def save_npz(self,filename):
+    def save_npz(self, filename):
         if not os.path.exists(filename):
-            np.savez(filename, **(self.__data))
-            
-            
+            np.savez(filename, **(self.data))
+
     def get_temp(self):
-        if 'Temperature' in self.__data.keys():
-            return self.__data['Temperature']
+        if "Temperature" in self.data.keys():
+            return self.data["Temperature"]
         else:
-            if 'InternalEnergy' in self.__data.keys() and 'ElectronAbundance' in self.__data.keys():
-                return 
+            if (
+                "InternalEnergy" in self.data.keys()
+                and "ElectronAbundance" in self.data.keys()
+            ):
+                return
+
+    def load_rot(self, rotation_data):
+        if self.__rotation_ok:
+            positions = np.array([self.data["x"], self.data["y"], self.data["z"]])
+            self.data["x"], self.data["y"], self.data["z"] = rotation_data @ positions
+            self.__rotation_record.append(rotation_data)
+
+    def see_rot(self):
+        if self.__rotation_ok:
+            a = np.eye(3)
+            for i in self.__rotation_record:
+                a = a @ i
+            return a
+
+    def cut_data(self, n):
+        for i in self.data.keys():
+            self.data[i] = self.data[i][:n]
+        print("Cut data.")
+
+    def calc_rot(self, n=400_000):
+        if self.__rotation_ok and "Masses" in self.keys():
+            if n > len(self.data["Masses"]):
+                n = len(self.data["Masses"])
+            else:
+                pass
+            x, y, z, m = (
+                self.data["x"][:n],
+                self.data["y"][:n],
+                self.data["z"][:n],
+                self.data["Masses"][:n],
+            )
+
+            # Calculate the moment of inertia tensor
+            I = np.zeros((3, 3))
+            I[0][0] = np.sum(m * (y**2 + z**2))
+            I[1][1] = np.sum(m * (x**2 + z**2))
+            I[2][2] = np.sum(m * (x**2 + y**2))
+            I[0][1] = -1 * np.sum(m * (x * y))
+            I[0][2] = -1 * np.sum(m * (x * z))
+            I[1][2] = -1 * np.sum(m * (y * z))
+            I[1][0] = I[0][1]
+            I[2][0] = I[0][2]
+            I[2][1] = I[1][2]
+
+            # Solve the eigenvalue equation and derive the rotation matrix
+            eigen_values, rotation_matrix = np.linalg.eig(I)
+            sort_inds = np.argsort(eigen_values)
+            eigen_values = eigen_values[sort_inds]
+            R = np.array(
+                (
+                    rotation_matrix[sort_inds[0]],
+                    rotation_matrix[sort_inds[1]],
+                    rotation_matrix[sort_inds[2]],
+                )
+            )
+            return R
+        else:
+            raise ValueError("To use this method, Masses must be in the data in order to calculate the moment of inertia tensor.\nIt is possible to compute another physical quantity disguised as Masses, but this is a deprecated practice.")
+    
+    def set_faceon(self):
+        if self.__rotation_ok:
+            positions = np.array([self.data["x"], self.data["y"], self.data["z"]])
+            self.data["x"], self.data["y"], self.data["z"] = self.calc_rot() @ positions
+    
+    def set_edgeon(self):
+        if self.__rotation_ok:
+            positions = np.array([self.data["x"], self.data["y"], self.data["z"]])
+            self.data["x"], self.data["y"], self.data["z"] = self.calc_rot() @ positions
+            self.rot_x(90)
+
+
+class manage_subhalo:
+    def __init__(self, basePath):
+        self.files_list = glob.glob(basePath + "/*")
+        self.files_list.sort()
